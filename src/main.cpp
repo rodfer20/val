220017639d104c142ade8502efd05a07be0db689
rodrigolf <rodrigolf@hackintosh.home>
2022-06-19 23:40:23 +0100

    deprecated included dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <stdlib.h>
-
-#include "../include/val/renderer.hpp"
+#include "SDL.h"
+#include "renderer.hpp"
 
 int main(void)
 {
-    Renderer renderer = Renderer();
-    renderer.printCreate();
+    Renderer rnd = Renderer();
+    rnd.printCreate();
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "val",
+        0,
+        0,
+        1080,
+        640,
+        1
+    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
     return EXIT_SUCCESS;
 }
